324a5e8d31a7fa4a72332bc856d71473d92107e8
jt <jt@pop-os.localdomain>
2019-08-16 12:49:48 -0500

    also colors

--- src/components/Dashboard/DashboardLayout.js
+++ src/components/Dashboard/DashboardLayout.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
 
-const { Content, Sider } = Layout
+const { Content, Sider, Footer } = Layout
 
 class DashboardLayout extends Component {
 
   render() {
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider >
+        <Sider style={{ backgroundColor: "#fff", position: "fixed"}}>
           <h1>My Sider</h1>
         </Sider>
         <Layout>
-          <Content >
-            <div style={{ minHeight: 360 }}>
+          <Content style={{ backgroundColor: "#fff" }}>
+            <div style={{ minHeight: 360, textAlign: "center" }}>
               Bill is a cat.
             </div>
           </Content>
+          <Footer style={{ textAlign: 'center' }}><hr />Designed with Ant Design <br /> Powered by Mayan EDMS</Footer>
         </Layout>
       </Layout>
     );
