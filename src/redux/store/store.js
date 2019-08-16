6020a110cebafda28eaf3e2f4e465612c695fb21
jt <jt@pop-os.localdomain>
2019-08-16 01:22:24 -0500

    loadstate and save state from and to localstorage

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,14 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
 import { allReducers } from '../Reducers/';
+import { loadState, saveState }  from './localStorage';
+
+const persistedState = loadState()
 
 const myMiddleware = [thunk];
 
 export const store = createStore(
   allReducers,
+  persistedState,
   compose(
     applyMiddleware(...myMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+store.subscribe(() => {
+  saveState({
+    auth: store.getState().auth
+  });
+});
