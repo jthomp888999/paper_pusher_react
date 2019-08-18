53c128cc38163d714a487221eb6ff2e4230f2f55
jt <jt@pop-os.localdomain>
2019-08-17 21:46:37 -0500

    fix PrivateRoute to actually check for isAuth..

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
+const state = JSON.parse(localStorage.getItem('state'));
+
+const isAuthenticated = state.auth.isAuthenticated;
+
 export const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route 
   {...rest} 
-    render={props => localStorage.getItem('state') ? (
+    render={props => isAuthenticated ? (
       <Component {...props} />
       ) : (
         <Redirect to="/login"/>
@@ -12,3 +16,4 @@ export const PrivateRoute = ({ component: Component, ...rest }) => (
   }
   />
 );
+
