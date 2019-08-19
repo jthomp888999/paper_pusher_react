640b3ff32eb3d9067b493980041c6a556dcfe8be
jthomp <jthomp@protonmail.com>
2019-08-19 16:57:05 -0500

    moving layour rendering to route itself

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import DashboardLayout from "../components/Dashboard/DashboardLayout";
 
-const state = JSON.parse(localStorage.getItem('state'));
+const state = JSON.parse(localStorage.getItem("state"));
 
 const isAuthenticated = state.auth.isAuthenticated;
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route 
-  {...rest} 
-    render={props => isAuthenticated ? (
-      <Component {...props} />
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated ? (
+        <DashboardLayout>
+          <Component {...props} />
+        </DashboardLayout>
       ) : (
-        <Redirect to="/login"/>
-        )
-  }
+        <Redirect to="/login" />
+      )
+    }
   />
 );
-
