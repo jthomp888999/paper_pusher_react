f3963a8b9b914ce7e4bf14775cc476e9629e3989
jthomp <jthomp@protonmail.com>
2019-08-19 16:56:33 -0500

    testing new dashboard layout rendering

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
-import DashboardLayout from "./DashboardLayout";
-import DashRoutes from "./DashRoutes/DashRoutes";
 
 class Dashboard extends Component {
   render() {
-    return (
-      <DashboardLayout>
-        <h1>Dashboard</h1>
-        <DashRoutes />
-      </DashboardLayout>
-    );
+    return <h1>Dashboard</h1>;
   }
 }
 
