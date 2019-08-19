29890202705b43e18720a0fd398240bf581687dd
jthomp <jthomp@protonmail.com>
2019-08-19 16:56:47 -0500

    rearnged order to make sense

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { PrivateRoute } from '../Routes/PrivateRoute';
-import Dashboard from '../components/Dashboard/Dashboard';
-import Login from '../components/Login/Login';
-import Logout from '../components/Login/Logout';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { PrivateRoute } from "../Routes/PrivateRoute";
+import Dashboard from "../components/Dashboard/Dashboard";
+import Login from "../components/Login/Login";
+import Logout from "../components/Login/Logout";
 
 class routes extends Component {
   render() {
     return (
       <Switch>
-        <PrivateRoute exact path="/" component={Dashboard} />
         <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/" component={Dashboard} />
         <PrivateRoute exact path="/logout" component={Logout} />
       </Switch>
     );
