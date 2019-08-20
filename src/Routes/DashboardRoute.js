8421254d8c288689c637e690c16c134f63e063e8
jt <jt@pop-os.localdomain>
2019-08-20 13:41:26 -0500

    formating

--- src/Routes/DashboardRoute.js
+++ src/Routes/DashboardRoute.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import MainLayout from '../Layout/MainLayout'
+
 const state = JSON.parse(localStorage.getItem("state"));
 
 const isAuthenticated = state.auth.isAuthenticated;
