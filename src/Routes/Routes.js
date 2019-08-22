be386660ee673ad5d637a24430201be9b9668f38
jthomp <jthomp@protonmail.com>
2019-08-22 04:02:38 -0500

    merge damn

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import { DashboardRoute } from "./DashboardRoute";
 import Dashboard from "../components/Dashboard/Dashboard";
-import CabinetContents from "../components/Dashboard/DashComponents/Cabinets";
 import Login from "../components/Login/Login";
 import Logout from "../components/Login/Logout";
-=======
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { DashboardRoute } from './DashboardRoute';
-import Dashboard from '../components/Dashboard/Dashboard';
-import Login from '../components/Login/Login';
-import Logout from '../components/Login/Logout';
-import CabinetContents from '../components/Dashboard/DashComponents/CabinetContents';
->>>>>>> ecfc2176c0e87b733d7a1b18b4cf499835f4136c
+import CabinetContents from "../components/Dashboard/DashComponents/CabinetContents";
 
 class routes extends Component {
   render() {
