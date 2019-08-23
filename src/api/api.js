88a593d153f7c056c657b80221e7ca808b84cd86
jthomp <jthomp@protonmail.com>
2019-08-23 11:43:59 -0500

    formatting

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 export var API = axios.create({
-  baseURL: 'http://localhost:80/api/',
+  baseURL: "http://localhost:80/api/",
   headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
+    "Content-Type": "application/json",
+    Accept: "application/json"
   }
 });
 
 export const tokenObtain = user => {
-  return API.post('rest/auth/token/obtain/', user);
+  return API.post("rest/auth/token/obtain/", user);
 };
 
 export const currentUser = () => {
   // Gets called right after tokenObtain, otherwise,
   // the next bit might be better there
-  const state = JSON.parse(localStorage.getItem('state'));
+  const state = JSON.parse(localStorage.getItem("state"));
   const token = state.auth.token;
-  API.defaults.headers.common['Authorization'] = `token ${token}`;
-  return API.get('user_management/users/current/');
+  API.defaults.headers.common["Authorization"] = `token ${token}`;
+  return API.get("user_management/users/current/");
 };
 
 export const cabinetObj = () => {
-  return API.get('cabinets/cabinets');
+  return API.get("cabinets/cabinets/");
 };
 
 export const docsInCabinet = id => {
