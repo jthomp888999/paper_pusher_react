7d1b70fd0e37fbae002f9a77299649b853682e01
jt <jthomp@protonmail.com>
2019-08-31 00:35:27 -0500

    renamed and fixed up a fewt things... adding proptypes

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import Navigation from './Navigation/Navigation';
+import CabinetTree from '../components/Dashboard/DashComponents/CabinetTree';
+
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
+
+const MainLayout = ({ children }) => (
+  <Layout>
+    <Header className="header">
+      <Navigation />
+    </Header>
+    <Content>
+      <Layout style={{ background: '#fff' }}>
+        {/* Left Menu for cabints */}
+        <Sider
+          width={280}
+          style={{
+            background: '#fff',
+            border: 'solid #F0F2F5 1px',
+            minHeight: '800px',
+          }}
+        >
+          <CabinetTree />
+        </Sider>
+        <Content
+          style={{
+            padding: '0 24px',
+            minHeight: 280,
+            textAlign: 'center',
+          }}
+        >
+          {children}
+        </Content>
+        {/* Right Menu for Menu.. */}
+        <Sider
+          width={280}
+          style={{
+            background: '#fff',
+            border: 'solid #F0F2F5 1px',
+            minHeight: '800px',
+          }}
+        />
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Created by John Thompson
+      {' '}
+      <br />
+      {' '}
+Powered by Mayan EDMS
+    </Footer>
+  </Layout>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MainLayout;
---
+++ src/components/Dashboard/DashComponents/CabinetContents.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Table } from 'antd';
+import { setHeaders, docsInCabinet } from '../../../api/api';
+
+const {
+  auth: { token },
+  match: {
+    params: { id }
+  }
+} = this.props;
+
+class CabinetContents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cabinetContents: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    setHeaders(this.props.auth.token);
+    this.getContents(this.props.match.params.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.getContents(this.props.match.params.id);
+    }
+  }
+
+  // Return list of all documents in cabinet
+  getContents = id => {
+    this.setState({ isLoading: true });
+    docsInCabinet(id).then(res => {
+      this.setState({ cabinetContents: res.data.results });
+    });
+    this.setState({ isLoading: false });
+  };
+
+  render() {
+    const { cabinetContents, isLoading } = this.state;
+    const columns = [
+      {
+        title: 'Date Added',
+        dataIndex: 'date_added',
+        key: 'date_added'
+      },
+      {
+        title: 'Title',
+        dataIndex: 'label',
+        key: 'label'
+      }
+    ];
+
+    if (isLoading) {
+      return <>Loading...</>;
+    }
+    return (
+      <>
+        <Table
+          rowKey={Math.random}
+          columns={columns}
+          dataSource={cabinetContents}
+        />
+      </>
+    );
+  }
+}
+
+CabinetContents.propTypes = {
+  auth: PropTypes.shape({
+    token: PropTypes.string.isRequired
+  }),
+  match: PropTypes.shape({
+    params: PropTypes.object.isRequired
+  })
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(CabinetContents);
