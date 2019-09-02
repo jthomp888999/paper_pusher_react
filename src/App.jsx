3a53bbab7cd84b1059bbf134507d67e2f923d66c
jt <jthomp@protonmail.com>
2019-09-02 00:20:10 -0500

    moved the css to top level app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store/store';
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'antd/dist/antd.css';
 import { Layout } from 'antd';
 import Navigation from './Navigation/Navigation';
 import CabinetTree from '../components/Dashboard/DashComponents/CabinetTree';
