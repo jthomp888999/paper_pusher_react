68d83068972e97f11c26170f0e779f5e6dba7c88
jthomp <jthomp@protonmail.com>
2019-09-03 00:23:53 -0500

    converting cabinet tree to react hooks

---
+++ src/components/Cabinets/CabinetTreeHooks.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Tree, Empty, Spin } from 'antd';
+import { withRouter } from 'react-router-dom';
+import { cabinetObj } from '../api';
+
+const CabinetTreeHooks = () => {
+  const { firstList, setFirst } = useState([]);
+  const { secondList, setSecond } = useState([]);
+  const { cabinets, setCabinets } = useState([]);
+  const { isLoading, setLoading } = useState(false);
+
+  // This is a WIP, will need to be a recursive function to handle more pages soon
+  fetchCabinetTree = () => {
+    if (localStorage.getItem('cabinets')) {
+      this.setCabinets({
+        cabinets: JSON.parse(localStorage.getItem('cabinets')),
+      });
+    } else {
+      cabinetObj().then(res => {
+        this.setFirst({ firstList: res.data.results });
+        if (res.data.next !== null) {
+          cabinetObj(2).then(res2 => {
+            this.setSecond({ secondList: res2.data.results });
+            this.setCabinets({
+              cabinets: [
+                ...this.state.firstList,
+                ...this.state.secondList,
+              ],
+            });
+            localStorage.setItem(
+              'cabinets',
+              JSON.stringify(this.state.cabinets),
+            );
+          });
+        }
+      });
+    }
+  };
+
+  return <div></div>;
+};
+
+export default CabinetTreeHooks;
