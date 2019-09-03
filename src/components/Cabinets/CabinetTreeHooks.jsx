5383b9addd68367a471590df3ff9ee3382aa242c
jt <jthomp@protonmail.com>
2019-09-03 12:17:54 -0500

    finished trying out a hooks component

--- src/components/Cabinets/CabinetTreeHooks.jsx
+++ src/components/Cabinets/CabinetTreeHooks.jsx
@@ -4,33 +4,32 @@ import { Tree, Empty, Spin } from 'antd';
 import { withRouter } from 'react-router-dom';
 import { cabinetObj } from '../api';
 
-const CabinetTreeHooks = () => {
+const { DirectoryTree, TreeNode } = Tree;
+
+const CabinetTreeHooks = props => {
   const { firstList, setFirst } = useState([]);
   const { secondList, setSecond } = useState([]);
   const { cabinets, setCabinets } = useState([]);
   const { isLoading, setLoading } = useState(false);
 
   // This is a WIP, will need to be a recursive function to handle more pages soon
-  fetchCabinetTree = () => {
+  const fetchCabinetTree = () => {
     if (localStorage.getItem('cabinets')) {
-      this.setCabinets({
+      setCabinets({
         cabinets: JSON.parse(localStorage.getItem('cabinets')),
       });
     } else {
       cabinetObj().then(res => {
-        this.setFirst({ firstList: res.data.results });
+        setFirst({ firstList: res.data.results });
         if (res.data.next !== null) {
           cabinetObj(2).then(res2 => {
-            this.setSecond({ secondList: res2.data.results });
-            this.setCabinets({
-              cabinets: [
-                ...this.state.firstList,
-                ...this.state.secondList,
-              ],
+            setSecond({ secondList: res2.data.results });
+            setCabinets({
+              cabinets: [...firstList, ...secondList],
             });
             localStorage.setItem(
               'cabinets',
-              JSON.stringify(this.state.cabinets),
+              JSON.stringify(cabinets),
             );
           });
         }
@@ -38,7 +37,69 @@ const CabinetTreeHooks = () => {
     }
   };
 
-  return <div></div>;
+  useEffect(() => {
+    setLoading(true);
+    fetchCabinetTree();
+    setLoading(false);
+  });
+
+  // Load contents in cernter body, will reset root page if any errors occur
+  const onSelect = (selectedKeys, info) => {
+    const { history } = props;
+    try {
+      history.push({
+        pathname: `/cabinets/${info.selectedNodes[0].props.id}`,
+        state: { id: info.selectedNodes[0].props.id },
+      });
+    } catch {
+      history.push('/');
+    }
+  };
+
+  const cleanCabinets = [];
+  // Must actually clean the data from the server to be actually nested
+  if (!isLoading) {
+    cabinets.map(item => {
+      if (item.children.length && item.parent === null) {
+        return cleanCabinets.push(item);
+      }
+      return cleanCabinets;
+    });
+  }
+
+  // Recursively build treenodes
+  const renderTreeNodes = data =>
+    data.map(item => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.label} id={item.id}>
+            {renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode item={item.label} id={item.id} isLeaf />;
+    });
+
+  // Conditionally render the cabinet tree
+  if (isLoading) {
+    return <Spin size="large" />;
+  }
+  if (cleanCabinets.length === 0) {
+    return <Empty description="Cabinets Empty" />;
+  }
+  return (
+    <>
+      <DirectoryTree onSelect={onSelect}>
+        {renderTreeNodes(cleanCabinets)}
+      </DirectoryTree>
+    </>
+  );
 };
 
-export default CabinetTreeHooks;
+CabinetTreeHooks.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(CabinetTreeHooks);
